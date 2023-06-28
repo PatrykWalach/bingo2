92a300e070346904b6b967fd74bdbfb09083aba2
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2023-06-28 10:36:30 +0200

    feat: setup houdini

---
+++ houdini.config.js
@@ -0,0 +1,11 @@
+/// <references types="houdini-svelte">
+
+/** @type {import('houdini').ConfigFile} */
+const config = {
+    "schemaPath": "schema.graphql",
+    "plugins": {
+        "houdini-svelte": { framework:'kit'}
+    }
+}
+
+export default config
