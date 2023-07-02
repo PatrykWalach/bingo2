9a75fbcd4d481597f0c23a4906fcc16661a00a6f
PatrykWalach <PatrykWalach@users.noreply.github.com>
2023-07-02 14:20:13 +0000

    refactor: run format

--- src/lib/socket.client.ts
+++ src/lib/socket.client.ts
@@ -1,7 +1,7 @@
-import { browser } from '$app/environment';
-import { PUBLIC_PUSHER_KEY } from '$env/static/public';
-import Pusher from 'pusher-js';
-import { socketId } from './socket';
+import { browser } from '$app/environment'
+import { PUBLIC_PUSHER_KEY } from '$env/static/public'
+import Pusher from 'pusher-js'
+import { socketId } from './socket'
 
 export const pusher = new Pusher(PUBLIC_PUSHER_KEY, {
 	cluster: 'sa1'
