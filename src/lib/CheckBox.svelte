93a94e998f123fd08deb69b07b36d48477078b69
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2023-07-05 19:09:14 +0200

    fix: lint

--- src/lib/CheckBox.svelte
+++ src/lib/CheckBox.svelte
@@ -2,12 +2,16 @@
 	import type { Writable } from 'svelte/store'
 	import type { FormPathLeaves, ZodValidation } from 'sveltekit-superforms'
 	import { formFieldProxy, type SuperForm } from 'sveltekit-superforms/client'
-	import type { z } from 'zod'
+	import type { AnyZodObject, z } from 'zod'
 
+	/* eslint-disable-next-line @typescript-eslint/no-unused-vars, no-undef */
 	type T = $$Generic<AnyZodObject>
 
 	export let form: SuperForm<ZodValidation<T>, unknown>
 	export let field: FormPathLeaves<z.infer<T>>
+	export let id = String(field)
+
+	$: inputId = id ?? String(field)
 	const { value, errors, constraints } = formFieldProxy(form, field)
 
 	$: boolValue = value as Writable<boolean>
@@ -17,6 +21,7 @@
 	<label class="label cursor-pointer">
 		<span class="label-text">Free tile</span>
 		<input
+			id={inputId}
 			name={field}
 			type="checkbox"
 			class="checkbox"
@@ -24,5 +29,10 @@
 			{...$constraints}
 			{...$$restProps}
 		/>
+	</label>
+	<label class="label" for={inputId}>
+		<span class="label-text-alt text-error">
+			{$errors || ''}
+		</span>
 	</label>
 </div>
